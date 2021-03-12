ff10c0d15de13dc5b66b4f42e607c5814d27dda0
sjung16 <sjung16@calstatela.edu>
2021-03-12 13:17:57 -0800

    Deleted uuid, password
    room_number is now the main parameter, instead of game_uuid

--- poker_db.py
+++ poker_db.py
@@ -1,4 +1,3 @@
-from uuid import uuid4
 from typing import List, Tuple, Dict, Union
 from poker.poker import Poker
 import asyncio
@@ -8,9 +7,10 @@ from dataclasses import dataclass
 
 @dataclass
 class PokerGameInfo:
+    room_number: str
     num_players: int
+    starting_cash: int
     players: List[str]
-    termination_password: str
 
 
 class AsyncPokerGameDB(object):
@@ -20,48 +20,51 @@ class AsyncPokerGameDB(object):
         self._QUERY_TIME: float = 0.05
         self._user_db = user_db
 
-    async def add_game(self, num_players: int, starting_cash: int = 1000) -> Tuple[str, str]:
+    async def add_game(self, room_number: str, num_players: int, starting_cash: int = 1000) -> str:
         """
         Asks the database to create a new game.
 
+        :param room_number: room number
         :param num_players: number of players
         :param starting_cash: amount of money each player starts with
-        :return: the UUID (universally-unique ID) of the game
+        :return: the room number of the game
         """
+        if room_number in self._current_games_info:
+            raise KeyError('That room number is taken.')
         await asyncio.sleep(self._QUERY_TIME)  # simulate query time
-        game_uuid = str(uuid4())
-        game_term_password = str(uuid4())
-        self._current_games[game_uuid] = Poker(num_players, starting_cash)
-        self._current_games_info[game_uuid] = PokerGameInfo(
+        self._current_games[room_number] = Poker(num_players, starting_cash)
+        self._current_games_info[room_number] = PokerGameInfo(
+            room_number,
             num_players,
+            starting_cash,
             list(),
-            game_term_password)
-        return game_uuid, game_term_password
+            )
+        return room_number
 
-    async def list_games(self) -> List[Tuple[str, int]]:
-        """
-        Asks the database for a list of all active games.
+    # async def list_games(self) -> List[Tuple[str, int]]:
+    #     """
+    #     Asks the database for a list of all active games.
+    #
+    #     :return: list of (game_id, number of players in game)
+    #     """
+    #     await asyncio.sleep(self._QUERY_TIME)  # simulate query time
+    #     return [(game_id, game._num_players) for game_id, game in self._current_games.items()]
 
-        :return: list of (game_id, number of players in game)
-        """
-        await asyncio.sleep(self._QUERY_TIME)  # simulate query time
-        return [(game_id, game._num_players) for game_id, game in self._current_games.items()]
-
-    async def get_game(self, game_id: str) -> Union[Poker, None]:
+    async def get_game(self, room_number: str) -> Union[Poker, None]:
         """
         Asks the database for a pointer to a specific game.
 
-        :param game_id: the UUID of the specific game
+        :param room_number: the room number
         :return: None if the game was not found, otherwise pointer to the Poker object
         """
         await asyncio.sleep(self._QUERY_TIME)  # simulate query time
-        return self._current_games.get(game_id, None)
+        return self._current_games.get(room_number, None)
 
-    async def get_game_info(self, game_id: str):
+    async def get_game_info(self, room_number: str):
         """
         Asks the database for num_players, list of players, and termination password for a specific game.
 
-        :param game_id: the UUID of the specific game
+        :param room_number: the room number of the specific game
         :return: list of players in the game game_id
         """
-        return self._current_games_info[game_id]
+        return self._current_games_info[room_number]
--- poker_mqtt.py
+++ poker_mqtt.py
@@ -2,6 +2,7 @@ import asyncio
 from asyncio_mqtt import Client, MqttError
 from poker_db import AsyncPokerGameDB
 from user_db import UserDB
+from dataclasses import dataclass
 
 USER_DB = UserDB()
 POKER_DB = AsyncPokerGameDB(USER_DB)
@@ -9,7 +10,7 @@ POKER_DB = AsyncPokerGameDB(USER_DB)
 
 async def message_handler():
     """
-    Runs the mqtt client and handles messages via topic filters.
+    Runs the MQTT client and handles messages via topic filters.
     """
     async with Client("localhost") as client:
         await client.subscribe("#")
@@ -17,31 +18,31 @@ async def message_handler():
             async for message in messages:
                 message_str = message.payload.decode()
                 if message_str.startswith("create_user"):
+                    # Delete the command portion of the message ("create_user")
                     message_params = message_str.replace("create_user", '')
+                    # Then feed the remaining message of only the parameter(s) (username).
                     await create_user(client, message_params)
                 elif message_str.startswith("create_game"):
                     message_params = message_str.replace("create_game", '')
                     await create_game(client, message_params)
-        # async with client.filtered_messages("game_command/create") as messages:
-        #     await client.subscribe("game_command/create")
-        #     async for message in messages:
-        #         await create_user(client, message)
+                # elif message_str.startswith("add_player"):
+                #     message_params = message_str.replace("add_player", '')
+                #     await add_player(client, message_params)
 
 
 async def create_user(client, message_params):
     """
     Adds a user with the input username to the user database, with a randomly generated password.
-    The user must publish a message of his desired username under topic "user_command/create".
+    The user must publish a message of his desired username under the designated topic and message format below.
+
+    Topic: "user_command/create"
+    Message format: "create_user username"
+
+    Example: User publishes string message "create_user john_doe" under topic "user_command/create" to create a new
+             user with username john_doe
 
     :return: The username and the password
     """
-    # async with Client("localhost") as client:
-    #     # Receive messages that match the given filter and store as messages
-    #     async with client.filtered_messages("user_command/create") as messages:
-    #         await client.subscribe("user_command/create")
-    #         async for message in messages:
-    #             # Convert bytes to string
-    #                 message_str = message.payload.decode()
     try:
         new_username, new_password = USER_DB.create_user(message_params)
     except ValueError:
@@ -53,38 +54,33 @@ async def create_user(client, message_params):
 
 async def create_game(client, message_params):
     """
-    Creates a game according to user input parameters.
-    The user must publish a message with the three input parameters under topic "game_command/create".
+    Creates a game according to user input parameters, and adds the game to the game database.
+    The user must publish a message of his desired username under the designated topic and message format below.
 
-    User input parameters: game_rooms, num_players, starting_cash
-                           (Important: parameters must be separated by a comma!)
+    Topic: "game_command/create"
+    Message format: "create_game game_number, num_players, starting_cash" (Important: the three parameters MUST be
+                    separated by a comma!"
 
-    Example: If the user publishes to topic "game_command/create" the message "3, 5, 2000", it will create a game with
-             number title 3, 5 players and starting cash of 2000 per player.
-
-    A game_uuid and termination password will be automatically generated for the game room.
+    Example: User publishes string message "create_game 3, 5, 2000" under topic "game_command/create" to create new game
+             with game_number=3, num_players=5, and starting_cash=2000
     """
-    # async with Client("localhost") as client:
-    #     # Receive messages that match the given filter and store as messages
-    #     async with client.filtered_messages("game_command/create") as messages:
-    #         await client.subscribe("game_command/create")
-    #         async for message in messages:
-    #             # Convert bytes to string
-    # Split the message string to three parameters: room number, num_players, and starting_cash
     message_split = message_params.split(",")
-    if message_split[0] in await client.subscribe("game_rooms"):
-        raise MqttError("That room number already exists.")
-    game_uuid, term_pass = await POKER_DB.add_game(num_players=int(message_split[1]),
-                                                   starting_cash=int(message_split[2]))
-    await client.publish(("game_rooms/" + str(message_split[0])) + "/game_uuid", game_uuid, qos=1)
-    await client.publish(("game_rooms/" + str(message_split[0])) + "/term_pass", term_pass, qos=1)
-    await client.publish(("game_rooms/" + str(message_split[0])) + "/create_success", "True", qos=1)
-    await client.publish(("game_rooms/" + str(message_split[0])) + "/num_players", message_split[1], qos=1)
-    await client.publish(("game_rooms/" + str(message_split[0])) + "/starting_cash", message_split[2], qos=1)
+    try:
+        await POKER_DB.add_game(room_number=str(message_split[0]),
+                                num_players=int(message_split[1]),
+                                starting_cash=int(message_split[2]))
+        game_info = await POKER_DB.get_game_info(message_split[0])
+        await client.publish(("game_rooms/" + str(message_split[0])) + "/create_success", "True", qos=1)
+        await client.publish(("game_rooms/" + str(message_split[0])) + "/num_players", game_info.num_players, qos=1)
+        await client.publish(("game_rooms/" + str(message_split[0])) + "/starting_cash", game_info.starting_cash, qos=1)
+    except KeyError:
+        await client.publish(("game_rooms/" + str(message_split[0])) + "/create_success",
+                             "False (detail: That game room already exists)", qos=1)
+        raise MqttError("That game room already exists.")
 
 
 async def main():
-    # Run example() indefinitely. Reconnect automatically if the connection is lost.
+    # Run the message handler indefinitely. Reconnect automatically if the connection is lost.
     reconnect_interval = 3  # [seconds]
     while True:
         try:
